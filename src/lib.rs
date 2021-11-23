9b06098c4cb5b5a4c24990e9dad54a292599291d
Misha Zharov <8266317+mishazharov@users.noreply.github.com>
2021-11-23 00:28:05 -0500

    Attempt to weed out unnessesary dependencies (#1)
    This PR removes dependencies which are not required to trigger the
    error. The process for obtaining this new dependency list was trial and
    error :P

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,3 @@
-use wasm_bindgen::prelude::*;
-use der;
-use der::Length;
-
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    let mut result = [0u8; 24];
-    let mut enc = der::Encoder::new(&mut result);
-    enc.sequence(Length::ZERO, |_| { Ok(()) }).unwrap();
-    alert(&format!("Hello, {}!", name));
+pub fn greet(_name: &str) {
+    println!("Hello world");
 }
